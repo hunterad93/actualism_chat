420a2b0fac82f43a1197fcbbdcac9c2a509c654a
hunterad93 <129822878+hunterad93@users.noreply.github.com>
2024-05-05 14:57:39 -0600

    initial commit

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ a fun chatbot that uses 'actualfreedom.com.au' as its knowledge base for retriev
 Specifically it uses the downloadable 'actual freedom lite' from this page: http://www.actualfreedom.com.au/sundry/aflite.htm
 
 The `knowledge_base_generator.ipynb` extracts all .htm files from this download, and converts them to html for compatabiity with the OpenAI vector store.
+
+The `db_cleanup.ipynb` notebook is a quick script that cleans up the database by removing any duplicate files. This is important for maintaining the integrity of the database and ensuring that only the most recent versions of each file are stored.
+
+The `streamlit_app/` directory contains the code for the web app that uses the assistant. It's a simple streamlit app that takes the user's input and sends it to the assistant. The assistant then returns the response and the app displays it. It includes support for 'streaming' responses which improves UI a lot.
+
---
+++ streamlit_app/app.py
@@ -0,0 +1,103 @@
+import openai
+import streamlit as st
+from openai import OpenAI
+import time
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+assistant_id = st.secrets["ASSISTANT_ID"]
+
+def ensure_single_thread_id():
+    if "thread_id" not in st.session_state:
+        thread = client.beta.threads.create()
+        st.session_state.thread_id = thread.id
+    return st.session_state.thread_id
+
+def get_filename(file_id):
+    try:
+        # Retrieve the file metadata from OpenAI
+        file_metadata = client.files.retrieve(file_id)
+        # Extract the filename from the metadata
+        filename = file_metadata.filename
+        return filename
+    except Exception as e:
+        print(f"Error retrieving file: {e}")
+        return None
+    
+def format_citation(annotation):
+    file_id = annotation.file_citation.file_id
+    filename = get_filename(file_id)
+    if filename:
+        citation_info = f"[Citation from {filename} "
+    else:
+        citation_info = "[Citation from an unknown file"
+    return citation_info
+
+def stream_generator(prompt, thread_id):
+    # Create the initial message
+    message = client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=prompt
+    )
+
+    # Start streaming the response
+    with st.spinner("Wait... Generating response..."):
+        stream = client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+            stream=True,
+            max_prompt_tokens=20000
+        )
+        partial_response = ""
+        for event in stream:
+            if event.data.object == "thread.message.delta":
+                for content in event.data.delta.content:
+                    if content.type == 'text':
+                        text_value = content.text.value
+                        annotations = content.text.annotations
+                        if annotations:
+                            for annotation in annotations:
+                                citation_info = format_citation(annotation)
+                                indexes = f"from index {annotation.start_index} to {annotation.end_index}]"
+                                text_value = f" **{citation_info + indexes}**"
+                        partial_response += text_value
+                        words = partial_response.split(' ')
+                        for word in words[:-1]:  # Yield all but the last incomplete word
+                            yield word + ' '
+                        partial_response = words[-1]  # Keep the last part for the next chunk
+            else:
+                pass
+        if partial_response:
+            yield partial_response  # Yield any remaining text
+
+# Streamlit interface
+st.set_page_config(page_icon="🌺")
+st.title("🌺 Discuss Actualism With ChatGPT")
+
+# Chat interface
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Enter your message")
+# Streamlit interface
+if prompt:
+    thread_id = ensure_single_thread_id()
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response_container = st.empty()  # Create an empty container for the response
+        full_response = ""
+        for chunk in stream_generator(prompt, thread_id):
+            full_response += chunk
+            # Update the container with the latest full response, adding fire emojis
+            response_container.markdown("🌺 " + full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
