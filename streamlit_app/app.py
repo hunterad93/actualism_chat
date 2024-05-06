126474f4e08eeb6741c65ceda03ac04064e6f572
hunterad93 <129822878+hunterad93@users.noreply.github.com>
2024-05-06 16:07:58 -0600

    update to use scraped db

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -28,7 +28,7 @@ def format_citation(annotation):
     filename = get_filename(file_id)
     if filename:
         # Replace '---' with '/' and '.html' with '.htm' for URL conversion
-        file_url = "http://www.actualfreedom.com.au/" + filename.replace('---', '/').replace('.md', '.htm')
+        file_url = "http://www.actualfreedom.com.au/" + filename.replace('---', '/').replace('.txt', '.htm')
         citation_info = f" ({file_url}) "
     else:
         citation_info = "[Citation from an unknown file]"
